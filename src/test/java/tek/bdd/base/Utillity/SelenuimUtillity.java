60145db0437d13ea6633dd667e619c3f3faface9
farhad <farhad.hakimi.1999@gmail.com>
2024-07-30 20:12:22 -0400

    first commit for SelenuimUtillity class and its methods

---
+++ src/test/java/tek/bdd/base/Utillity/SelenuimUtillity.java
@@ -0,0 +1,45 @@
+package tek.bdd.base.Utillity;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import tek.bdd.base.BaseSetUp;
+
+import java.time.Duration;
+
+public class SelenuimUtillity extends BaseSetUp {
+
+    private WebDriverWait getWait() {
+        return new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+    }
+
+    private WebElement waitForVisibility(By locator){
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    // create a methode to click on a given locator
+   public void clickOnElement(By locator){
+    getWait().until(ExpectedConditions.elementToBeClickable(locator))
+            .click();
+
+   }
+
+   public void sendText(By locator, String value){
+      getWait().until(ExpectedConditions.visibilityOfElementLocated(locator))
+              .click();
+   }
+
+   // method for getting the text of a locator
+
+    public String getElementText(By locator){
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).getText();
+
+   }
+
+
+
+
+
+
+}
